59acdfb2c99107e9f4aaaf2f7574d881ccdf4108
BRASNICA041 <BRASNICA041@samba.brasnica.com.br>
2025-09-05 07:50:02 -0300

    atualização da api para funcionar o site pulblico!

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 module.exports = {
   // Tenta usar a variável de ambiente JWT_SECRET. 
   // Se não existir, usa a string secreta como um fallback.
-  JWT_SECRET: process.env.JWT_SECRET || 'seu_segredo_super_secreto_e_longo_para_jwt'
+  JWT_SECRET: process.env.JWT_SECRET || 'd8f054308e3945399ab177c02d5e4389'
 };
